9a601eb70e1673dcff512cf4f45cf684e63615d7
fhuaquisto21 <fhuaquisto.aqp@gmail.com>
2022-06-29 10:28:50 -0500

    full screen

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(), "No botes la basura!", sf::Style::Fullscreen);
+    sf::View worldview(window.getDefaultView());
+    // Load a sprite to display
+    // Play the music}
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
